d36deeec95ee867cee5125649b7208b0b2ba5716
nati <natibekele@gmail.com>
2022-01-25 19:28:07 -0500

    temp stop blog builds

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,39 +7,40 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
-                }
-              }
-            }
-          }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
+    // TODO FIX UP BLOG
+    // const blogPost = path.resolve('./src/templates/blog-post.js')
+    // resolve(
+    //   graphql(
+    //     `
+    //       {
+    //         allContentfulBlogPost {
+    //           edges {
+    //             node {
+    //               title
+    //               slug
+    //             }
+    //           }
+    //         }
+    //       }
+    //     `
+    //   ).then(result => {
+    //     if (result.errors) {
+    //       console.log(result.errors)
+    //       reject(result.errors)
+    //     }
 
-        const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach(post => {
-          createPage({
-            path: `/blog/${post.node.slug}/`,
-            component: blogPost,
-            context: {
-              slug: post.node.slug,
-            },
-          })
-        })
-      })
-    )
+    //     const posts = result.data.allContentfulBlogPost.edges
+    //     posts.forEach(post => {
+    //       createPage({
+    //         path: `/blog/${post.node.slug}/`,
+    //         component: blogPost,
+    //         context: {
+    //           slug: post.node.slug,
+    //         },
+    //       })
+    //     })
+    //   })
+    // )
     const projectPost = path.resolve('./src/templates/project-post.js')
     resolve(
       graphql(
